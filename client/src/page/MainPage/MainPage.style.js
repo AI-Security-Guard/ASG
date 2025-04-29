b81569b097648581f51de4ec855436b593c29129
sojeong0302 <01duqrl@naver.com>
2025-04-29 21:39:13 +0900

    design: 메인 페이지 디자인 #7

--- client/src/page/MainPage/MainPage.style.js
+++ client/src/page/MainPage/MainPage.style.js
@@ -1,3 +1,45 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-export const MainContainer = styled.div``;
+const roll = keyframes`
+  0% {
+    transform: translateX(-200%) rotate(0deg);
+  }
+  100% {
+    transform: translateX(0) rotate(360deg);
+  }
+`;
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+export const MainContainer = styled.div`
+    height: 100vh;
+`;
+
+export const Logo = styled.div`
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const LogoImg = styled.img`
+    width: 20em;
+    animation: ${roll} 1s ease-out;
+`;
+
+export const LogoTxt = styled.div`
+    font-size: 3rem;
+    font-weight: bold;
+    opacity: 0;
+    animation: ${fadeIn} 1s ease-out forwards;
+    animation-delay: 1s;
+`;

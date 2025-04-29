3767837d42d6dcac2a593fa041c39f279b8c6f09
sojeong0302 <01duqrl@naver.com>
2025-04-29 16:40:17 +0900

    feat: 메인 페이지 생성 및 라우터 설정 #7

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import MainPage from './page/MainPage/MainPage.js';
 function App() {
     return (
         <>
             <Router>
-                <Routes></Routes>
+                <Routes>
+                    <Route path='/' element={<MainPage />} />
+                </Routes>
             </Router>
         </>
     );
---
+++ client/src/page/MainPage/MainPage.js
@@ -0,0 +1,11 @@
+import { MainContainer } from './MainPage.style.js';
+
+function MainPage() {
+    return (
+        <>
+            <MainContainer>메인 페이지</MainContainer>
+        </>
+    );
+}
+
+export default MainPage;

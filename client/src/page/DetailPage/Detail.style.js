72dfbe74cc6f0f1860997f0a2bd111be9e79c8dc
sojeong0302 <01duqrl@naver.com>
2025-05-04 23:07:28 +0900

    desgin: 초기 디자인 #16

--- client/src/page/DetailPage/Detail.style.js
+++ client/src/page/DetailPage/Detail.style.js
@@ -1,1 +1,60 @@
 import styled from 'styled-components';
+
+export const DetailContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+    justify-content: space-around;
+    height: 80%;
+    position: absolute;
+    bottom: 0;
+    gap: 5%;
+`;
+
+export const Detail = styled.div`
+    background-color: #b3b3b3;
+    border-radius: 10px;
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 3%;
+    height: 100%;
+    justify-content: space-around;
+`;
+
+export const TempVideo = styled.video`
+    width: 90%;
+    padding: 2%;
+    border: 1px solid black;
+    border-radius: 10px;
+    background-color: black;
+    height: 65%;
+`;
+
+export const DetailContent = styled.div`
+    background-color: #d9d9d9;
+    border: 1px solid black;
+    border-radius: 10px;
+    width: 90%;
+    height: 15%;
+    font-weight: bold;
+    font-size: 25px;
+    padding: 2%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+
+export const Date = styled.div``;
+
+export const Type = styled.div``;
+
+export const Location = styled.div``;
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: flex-end; /* 버튼을 오른쪽으로 정렬 */
+    width: 75%;
+`;
--- client/src/page/DetailPage/DetailPage.js
+++ client/src/page/DetailPage/DetailPage.js
@@ -1,7 +1,28 @@
-import {} from './Detail.style.js';
+import * as D from './Detail.style.js';
+import Header from '../../component/Header/Header.js';
+import ShortButton from '../../component/ShortButton/ShortButton.js';
 
 function DetailPage() {
-    return <></>;
+    return (
+        <>
+            <Header />
+            <D.DetailContainer>
+                <D.Detail>
+                    <D.TempVideo controls>
+                        <source src='/video.mp4' type='video/mp4' />
+                    </D.TempVideo>
+                    <D.DetailContent>
+                        <D.Date>발생시기: 2025 / 04 / 28 / 23:00:00</D.Date>
+                        <D.Type>수상행동 유형: 폭행</D.Type>
+                        <D.Location>최초 발생 위치: (123,123231)</D.Location>
+                    </D.DetailContent>
+                </D.Detail>
+                <D.ButtonContainer>
+                    <ShortButton txt='뒤로가기' />
+                </D.ButtonContainer>
+            </D.DetailContainer>
+        </>
+    );
 }
 
 export default DetailPage;

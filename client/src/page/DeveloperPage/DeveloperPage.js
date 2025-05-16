f4fe5b0152992e19dad110b0633c86699b2e2b9c
sojeong0302 <01duqrl@naver.com>
2025-05-17 01:29:24 +0900

    feat: 라우터 설정 #21

--- client/src/page/DeveloperPage/DeveloperPage.js
+++ client/src/page/DeveloperPage/DeveloperPage.js
@@ -1,7 +1,7 @@
 import { DeveloperContainer } from "./DeveloperPage.style.js";
 
 function DeveloperPage() {
-    return <DeveloperContainer></DeveloperContainer>;
+    return <DeveloperContainer>개발자 소개 페이지 입니다.</DeveloperContainer>;
 }
 
 export default DeveloperPage;

4e8af73a124dc506244127738b7cf7574d9713d3
sojeong0302 <01duqrl@naver.com>
2025-05-17 01:23:16 +0900

    feat: 개발자 소개 페이지 생성 #21

---
+++ client/src/page/DeveloperPage/DeveloperPage.js
@@ -0,0 +1,7 @@
+import { DeveloperContainer } from "./DeveloperPage.style.js";
+
+function DeveloperPage() {
+    return <DeveloperContainer></DeveloperContainer>;
+}
+
+export default DeveloperPage;

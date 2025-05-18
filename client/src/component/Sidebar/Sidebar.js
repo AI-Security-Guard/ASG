66335939a3fb3ad89ce5685256c194904fcd3c60
sojeong0302 <01duqrl@naver.com>
2025-05-19 02:22:42 +0900

    feat: 사이드바 클릭 #35

--- client/src/component/Sidebar/Sidebar.js
+++ client/src/component/Sidebar/Sidebar.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { SidebarContainer, SidebarButton, SidebarFooter } from "./Sidebar.style.js";
 import CameraAltIcon from "@mui/icons-material/CameraAlt";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 
 function Sidebar() {
+    const navigate = useNavigate();
+
     return (
         <SidebarContainer>
             <div>
-                <SidebarButton>
+                <SidebarButton onClick={() => navigate("/render")}>
                     <div className="content">
                         <CameraAltIcon fontSize="small" />
                         <span>CCTV 영상</span>
                     </div>
                 </SidebarButton>
-                <SidebarButton>
+                <SidebarButton onClick={() => navigate("/List")}>
                     <div className="content">
                         <AssignmentIcon fontSize="small" />
                         <span>기록</span>

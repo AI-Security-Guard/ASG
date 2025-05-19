3bd5a7f5e7245540ce627feefe89c2361bca755a
sojeong0302 <01duqrl@naver.com>
2025-05-19 19:59:18 +0900

    feat: 디자인 #43

--- client/src/page/RenderPage/RenderPage.style.js
+++ client/src/page/RenderPage/RenderPage.style.js
@@ -13,10 +13,40 @@ export const ContentArea = styled.div`
     width: 90%;
     height: 90%;
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
+    flex-direction: column;
 `;
 
 export const PlusIcon = styled.img`
     cursor: pointer;
+`;
+
+export const VideoPlayer = styled.video`
+    width: 60%;
+    border-radius: 12px;
+    box-shadow: 0 4px 10px rgba(0, 0, 0, 0.15);
+`;
+
+export const ButtonWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 80%;
+`;
+
+export const DeleteWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+`;
+
+export const DeleteVideo = styled.img`
+    cursor: pointer;
+    margin-right: 2%;
+    margin-bottom: 1%;
+    transition: transform 0.2s ease-in-out;
+
+    &:hover {
+        transform: scale(1.1);
+    }
 `;
